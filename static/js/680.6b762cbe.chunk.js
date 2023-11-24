fc4819dad72dfd403b1547eb738d0b5803595b40
Roman80-IT <127478871+Roman80-IT@users.noreply.github.com>
2023-11-24 22:14:08 +0000

    Deploying to gh-pages from @ Roman80-IT/goit-react-hw-08-phonebook@b6f4696127b12da67d7d7dc5cc7955e08e96c8ec 🚀

---
+++ static/js/680.6b762cbe.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,o){o.r(t);o(2791);var c=o(3329);t.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"PhoneBook"}),(0,c.jsx)("p",{children:"Welcome to the contact book!"})]})}}}]);
+//# sourceMappingURL=680.6b762cbe.chunk.js.map
